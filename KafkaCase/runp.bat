2f6b88feed0693ced5175e11ae1ff26b04e94738
Denise Case <dcase@nwmissouri.edu>
2018-02-27 15:07:30 -0600

    update

---
+++ KafkaCase/runp.bat
@@ -0,0 +1,1 @@
+java -cp target/KafkaCase-1.0-SNAPSHOT-jar-with-dependencies.jar edu.nwmissouri.isl.professorcase.kafka.CustomProducer 
---
+++ start-kafka.bat
@@ -0,0 +1,1 @@
+C:\ProgramData\chocolatey\lib\kafka\tools\kafka_2.11-1.0.0\bin\windows\kafka-server-start.bat C:\ProgramData\chocolatey\lib\kafka\tools\kafka_2.11-1.0.0\config\server.properties
